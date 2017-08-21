8a0141c21f07cdc7059764ad7b1f81b2fc40f76b
Alex <aldrake87@users.noreply.github.com>
2017-08-21 19:37:34 +0100

    Update processData.R

--- udf/processData.R
+++ udf/processData.R
@@ -3,8 +3,6 @@ library(tidyverse)
 #library(data.table) #<--- you'll need to install this!
 
 processData <- function(df = NULL){
-  ## processData expects a dataframe as an input, preferably the one you've just created using getData(). 
-  ## If a dataframe is found then user will see "Processing Data", otherwise "Data Error" will be returned.
   
   # check data is there
   if(!is.null(df)){
@@ -14,23 +12,7 @@ processData <- function(df = NULL){
       
       df <- df
       
-      # detect where stops have changed using data.table:::uniquelist
-      stopChange <- data.table:::uniqlist(df[,c("LAST_STOP_SHORT_DESC","CONTRACT_ROUTE","REGISTRATION_NUMBER")])
-      
-      # add detected column in
-      df[stopChange,"STOP_CHANGE"] <- "Yes"
-      df$STOP_CHANGE[is.na(df$STOP_CHANGE)] <- "No"
-      
-      # get rows where buses are stuck
-      stuck <- grep("No", df$STOP_CHANGE)
-      
-      # create summary stats on the stuck buses
-      dfStuck <- df[(stuck-1),] %>%
-        group_by(LAST_STOP_SHORT_DESC, CONTRACT_ROUTE) %>%
-        summarise(COUNT = length(CONTRACT_ROUTE))
-      
-      # remove stuck bus data
-      df <- df[-(stuck-1),]
+      df <- left_join(df, busSeq[,c("Route","Stop_Code_LBSL","Sequence","Run")], by=c("CONTRACT_ROUTE"="Route","LAST_STOP_SHORT_DESC"="Stop_Code_LBSL"))
       
       # detect change in reg and route
       regRoute <- data.table:::uniqlist(df[,c("CONTRACT_ROUTE","REGISTRATION_NUMBER")])
@@ -40,70 +22,73 @@ processData <- function(df = NULL){
       
       # re-align columns
       df[colInd,"SD2"] <- df[colInd-1,"SCHEDULE_DEVIATION"]
+      df[colInd,"TIME2"] <- df[colInd-1,"TIME_OF_DATA"]
+      df[colInd,"prevStop"] <- df[colInd-1,"Sequence"]
+      df[colInd,"prevRun"] <- df[colInd-1,"Run"]
+      df$TIME2 <- as.POSIXct.numeric(df$TIME2, origin = "1970-01-01", tz="GMT")
+      
+      # drop entries without previous run or where run has changed
+      df <- df[!is.na(df$prevRun),]
+      df <- df[df$Run == df$prevRun,]
+      df$prevRun <- NULL
       
       # calc change (reverse as +ve deviation means ahead of schedule by x)
       df$SD_CHANGE <- df$SCHEDULE_DEVIATION - df$SD2
-      df$SD_CHANGE[is.na(df$SD_CHANGE)] <- 0
       df$SD2 <- NULL
       
-      # remove data over 2 hours old
-      pd <- df[df$TIME_OF_DATA >= max(df$TIME_OF_DATA)-120*60,]
+      # calc time difference between records (for speed calcs)
+      df$timeDiff <- difftime(df$TIME_OF_DATA, df$TIME2, units="mins")
+      df$TIME2 <- NULL
       
-      # round to minutes and create observations column
-      pd$AVG_DELAY <- pd$SD_CHANGE/60
-      pd$S_DEV <- pd$SCHEDULE_DEVIATION/60
-      pd$OBS <- 1
+      # calc number of stops between records
+      df$prevStop[is.na(df$prevStop)] <- df$Sequence[is.na(df$prevStop)]
+      df$noStops <- (df$Sequence - df$prevStop) + 1
+      
+      # calculate the step change and remove dodgy data
+      df$stepChange <- df$SD_CHANGE / df$noStops
+      df$stepChange[df$noStops <= 1] <- NA
+      df$stepChange[df$noStops > mean(df$noStops[df$noStops >= 0], na.rm=T)+3*sd(df$noStops[df$noStops >= 0], na.rm=T)] <- NA
+      df <- df[!is.na(df$stepChange),]
+      
+      # remove data where bus has dropped further behind than expected
+      df$SD_CHANGE <- df$SD_CHANGE / 60
+      df <- df[abs(df$SD_CHANGE) < df$timeDiff,]
+      
+      # create data frame with filled gaps
+      pd <- data.frame(timeOfData = rep(df$TIME_OF_DATA, df$Sequence - df$prevStop + 1),
+                       route = rep(df$CONTRACT_ROUTE, df$Sequence - df$prevStop + 1),
+                       reg = rep(df$REGISTRATION_NUMBER, df$Sequence - df$prevStop + 1),
+                       run = rep(df$Run, df$Sequence - df$prevStop + 1),
+                       stepChange = rep(df$stepChange, df$Sequence - df$prevStop + 1),
+                       sequence = unlist(mapply(seq, df$prevStop, df$Sequence)))
+      
+      # round to minutes
+      pd$avgDelay <- pd$stepChange / 60
       
       # add weighting for TOD and calculate weighted delay
-      pd$Weight <- exp(-(as.numeric(difftime(max(pd$TIME_OF_DATA), pd$TIME_OF_DATA, units = "mins"))/120))
-      pd$AVG_DELAY_W <- as.numeric(pd$AVG_DELAY * pd$Weight)
+      pd$weight <- exp(-(as.numeric(difftime(max(pd$timeOfData),pd$timeOfData, units = "mins")) / 15))
+      pd$avgDelayW <- as.numeric(pd$avgDelay * pd$weight)
       
-      # select key columns and spread the time of data column into new column headers
+      # group by unique route/run/sequence to calculate vals
       tmp <- pd %>%
-        select(TIME_OF_DATA, AVG_DELAY, AVG_DELAY_W, S_DEV, LAST_STOP_SHORT_DESC, CONTRACT_ROUTE, OBS, Weight) %>%
-        group_by(LAST_STOP_SHORT_DESC, CONTRACT_ROUTE) %>%
-        summarise(weightSum = sum(Weight, na.rm = T),
-                  weightDelaySum = sum(AVG_DELAY_W, na.rm = T),
-                  obs = sum(OBS, na.rm = T),
-                  last_update = max(TIME_OF_DATA, na.rm = T)
-        )
+        group_by(route, run, sequence) %>%
+        summarise(weightSum = sum(weight, na.rm = T),
+                  weightDelaySum = sum(avgDelayW, na.rm = T),
+                  lastUpdate = max(timeOfData, na.rm = T))
       
       # calculate average delay
       tmp$avgDelay <- tmp$weightDelaySum / tmp$weightSum
       
-      # join 'stuck' observations
-      tmp <- left_join(tmp, dfStuck, by=c("LAST_STOP_SHORT_DESC"="LAST_STOP_SHORT_DESC","CONTRACT_ROUTE"="CONTRACT_ROUTE"))
-      tmp$COUNT[is.na(tmp$COUNT)] <- 0
-      tmp$obs <- tmp$obs + tmp$COUNT
-      tmp$COUNT <- NULL
-      
-      # open bus sequence sample and join to calculated data
-      bus.seq <- busSeq
-      bus.seq$Route <- as.character(bus.seq$Route)
-      bus.seq <- left_join(bus.seq, tmp, by=c("Route"="CONTRACT_ROUTE","Stop_Code_LBSL"="LAST_STOP_SHORT_DESC"))
+      # join bus sequence sample to calculated data
+      bus.seq <- left_join(busSeq, tmp, 
+                           by=c("Route"="route",
+                                "Run" = "run",
+                                "Sequence" = "sequence"))
       
       # order bus.seq by route, run and sequence
       bus.seq <- bus.seq[order(bus.seq$Route,bus.seq$Run,bus.seq$Sequence),]
       
-      # detect change in reg and route
-      runRoute <- data.table:::uniqlist(bus.seq[,c("Route","Run")])
-      
-      # replace first NA with 0 if it occurs in position 1
-      bus.seq[runRoute[is.na(bus.seq[runRoute,"avgDelay"])],"avgDelay"] <- 0
-      
-      # replace last NA with 0 if it occurs at the end of the run
-      runRoute <- runRoute-1
-      runRoute <- runRoute[runRoute > 0]
-      bus.seq[runRoute[is.na(bus.seq[runRoute,"avgDelay"])],"avgDelay"] <- 0
-      
-      # check last entry, set to 0 if na
-      pos <- length(bus.seq$avgDelay)
-      if(is.na(bus.seq$avgDelay[pos])){
-        bus.seq$avgDelay[pos] <- 0
-      }
-      
-      # interpolate NA values
-      bus.seq$avgDelay <- zoo::na.approx(bus.seq$avgDelay)
+      bus.seq$avgDelay[is.na(bus.seq$avgDelay)] <- 0
       
       # create cumulative sum dummy column
       bus.seq$cumSum <- NA
@@ -111,7 +96,7 @@ processData <- function(df = NULL){
       # order bus.seq by route, run and sequence
       bus.seq <- bus.seq[order(bus.seq$Route,bus.seq$Run,bus.seq$Sequence),]
       
-      # loop through each route and run to calculate cumulative sum
+      #start <- Sys.time()
       for(i in unique(bus.seq$Route)){
         for(j in unique(bus.seq$Run[bus.seq$Route == i])){
           
@@ -119,6 +104,11 @@ processData <- function(df = NULL){
           bus.seq$cumSum[bus.seq$Route == i & bus.seq$Run == j] <- cumsum(bus.seq$avgDelay[bus.seq$Route == i & bus.seq$Run == j])
         }
       }
+      
+      names(bus.seq) <- c("route","run","sequence","stopCodeLBSL","busStopCode","naptanAtco",
+                          "stopName","easting","northing","heading","virtualBusStop","fromStop",
+                          "toStop","direction","avgDist","weightSum","avgWeight","weightDelaySum",
+                          "lastUpdate","avgDelay","cumSum")
       
       # drop the log
       return(df = bus.seq)
