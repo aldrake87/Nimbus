2590b2473499f5ac7bde412b8cab8bb9ee35d21e
Alex <aldrake87@users.noreply.github.com>
2017-08-29 09:30:42 +0100

    Update processData.R

--- udf/processData.R
+++ udf/processData.R
@@ -25,15 +25,18 @@ processData <- function(df = NULL){
       df[colInd,"TIME2"] <- df[colInd-1,"TIME_OF_DATA"]
       df[colInd,"prevStop"] <- df[colInd-1,"Sequence"]
       df[colInd,"prevRun"] <- df[colInd-1,"Run"]
+      df[colInd,"prevTrip"] <- df[colInd-1,"TRIP_TYPE"]
       df$TIME2 <- as.POSIXct.numeric(df$TIME2, origin = "1970-01-01", tz="GMT")
       
       # drop entries without previous run or where run has changed
-      df <- df[!is.na(df$prevRun),]
-      df <- df[df$Run == df$prevRun,]
+      df <- df[!is.na(df$prevRun) &
+                 (df$TRIP_TYPE == 3 & df$prevTrip %in% c(2,3)) &
+                 df$Run == df$prevRun,]
       df$prevRun <- NULL
+      df$prevTrip <- NULL
       
       # calc change (reverse as +ve deviation means ahead of schedule by x)
-      df$SD_CHANGE <- df$SCHEDULE_DEVIATION - df$SD2
+      df$SD_CHANGE <- df$SD2 - df$SCHEDULE_DEVIATION
       df$SD2 <- NULL
       
       # calc time difference between records (for speed calcs)
@@ -55,15 +58,25 @@ processData <- function(df = NULL){
       df <- df[abs(df$SD_CHANGE) < df$timeDiff,]
       
       # create data frame with filled gaps
-      pd <- data.frame(timeOfData = rep(df$TIME_OF_DATA, df$Sequence - df$prevStop + 1),
-                       route = rep(df$CONTRACT_ROUTE, df$Sequence - df$prevStop + 1),
-                       reg = rep(df$REGISTRATION_NUMBER, df$Sequence - df$prevStop + 1),
-                       run = rep(df$Run, df$Sequence - df$prevStop + 1),
-                       stepChange = rep(df$stepChange, df$Sequence - df$prevStop + 1),
+      pd <- data.frame(timeOfData = rep(df$TIME_OF_DATA, df$noStops),
+                       route = rep(df$CONTRACT_ROUTE, df$noStops),
+                       reg = rep(df$REGISTRATION_NUMBER, df$noStops),
+                       run = rep(df$Run, df$noStops),
+                       stepChange = rep(df$stepChange, df$noStops),
                        sequence = unlist(mapply(seq, df$prevStop, df$Sequence)))
+      
+      # drop duplicate route, reg, run, sequence (ie only give latest result)
+      pd <- pd %>%
+        group_by(route, reg, run, sequence) %>%
+        summarise(time_Of_Data = max(timeOfData, na.rm = T),
+                  step_Change = sum(stepChange, na.rm = T))
+      
+      pd <- pd[,c("time_Of_Data", "route", "reg", "run", "step_Change", "sequence")]
+      names(pd) <- c("timeOfData","route","reg","run","stepChange","sequence")
       
       # round to minutes
       pd$avgDelay <- pd$stepChange / 60
+      pd$observations <- 1
       
       # add weighting for TOD and calculate weighted delay
       pd$weight <- exp(-(as.numeric(difftime(max(pd$timeOfData),pd$timeOfData, units = "mins")) / 15))
@@ -73,8 +86,10 @@ processData <- function(df = NULL){
       tmp <- pd %>%
         group_by(route, run, sequence) %>%
         summarise(weightSum = sum(weight, na.rm = T),
+                  avgWeight = mean(weight, na.rm = T),
                   weightDelaySum = sum(avgDelayW, na.rm = T),
-                  lastUpdate = max(timeOfData, na.rm = T))
+                  lastUpdate = max(timeOfData, na.rm = T),
+                  obs = sum(observations, na.rm = T))
       
       # calculate average delay
       tmp$avgDelay <- tmp$weightDelaySum / tmp$weightSum
@@ -96,19 +111,13 @@ processData <- function(df = NULL){
       # order bus.seq by route, run and sequence
       bus.seq <- bus.seq[order(bus.seq$Route,bus.seq$Run,bus.seq$Sequence),]
       
-      #start <- Sys.time()
-      for(i in unique(bus.seq$Route)){
-        for(j in unique(bus.seq$Run[bus.seq$Route == i])){
-          
-          # calc cumsum for each route/run combo
-          bus.seq$cumSum[bus.seq$Route == i & bus.seq$Run == j] <- cumsum(bus.seq$avgDelay[bus.seq$Route == i & bus.seq$Run == j])
-        }
-      }
+      # calc cumsum for each route/run combo
+      bus.seq$cumSum <- ave(bus.seq$avgDelay, bus.seq[,c("Route","Run")], FUN=cumsum)
       
       names(bus.seq) <- c("route","run","sequence","stopCodeLBSL","busStopCode","naptanAtco",
                           "stopName","easting","northing","heading","virtualBusStop","fromStop",
                           "toStop","direction","avgDist","weightSum","avgWeight","weightDelaySum",
-                          "lastUpdate","avgDelay","cumSum")
+                          "lastUpdate","obs","avgDelay","cumSum")
       
       # drop the log
       return(df = bus.seq)
